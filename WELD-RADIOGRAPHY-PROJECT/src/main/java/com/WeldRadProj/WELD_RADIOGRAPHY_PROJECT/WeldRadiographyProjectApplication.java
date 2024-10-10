97bebf367a6ab1f9062bf64af5bd1222ba793689
Gabriel Moreno Serignolli <tayseri@hotmail.com>
2024-10-10 20:27:08 -0300

    Adding SpringBoot

---
+++ WELD-RADIOGRAPHY-PROJECT/src/main/java/com/WeldRadProj/WELD_RADIOGRAPHY_PROJECT/WeldRadiographyProjectApplication.java
@@ -0,0 +1,13 @@
+package com.WeldRadProj.WELD_RADIOGRAPHY_PROJECT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeldRadiographyProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeldRadiographyProjectApplication.class, args);
+	}
+
+}
---
+++ WELD-RADIOGRAPHY-PROJECT/src/test/java/com/WeldRadProj/WELD_RADIOGRAPHY_PROJECT/WeldRadiographyProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WeldRadProj.WELD_RADIOGRAPHY_PROJECT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeldRadiographyProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
